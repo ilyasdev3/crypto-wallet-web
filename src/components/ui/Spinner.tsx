844404647030e786cbf79740175e9d7e2f1c4086
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-11-30 20:40:34 +0500

    Add spinner component

---
+++ src/components/ui/Spinner.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-primary-500"></div>
+    </div>
+  );
+};
+
+export default Spinner;
