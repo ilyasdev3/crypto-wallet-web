75dfc3c1f8e679c69eaf248a72cc54c7ac817b0e
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-11-30 20:42:59 +0500

    Update user mutation

--- src/graphql/user/mutation.user.ts
+++ src/graphql/user/mutation.user.ts
@@ -1,8 +1,17 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_USER = gql`
-  mutation CreateUser($user: CreateUserInput!) {
+  mutation Mutation($user: UserInput!) {
     createUser(user: $user) {
+      message
+      token
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation Mutation($user: UserInput!) {
+    loginUser(user: $user) {
       message
       token
     }
