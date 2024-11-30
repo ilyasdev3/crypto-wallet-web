dfbba2beeaab9addf69bae27c37426013d12e181
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-11-30 20:42:37 +0500

    Add graphql client

--- src/graphqlClient.ts
+++ src/graphqlClient.ts
@@ -1,12 +1,45 @@
 // src/apolloClient.ts
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "http://localhost:4000/graphql",
-    credentials: "include",
-  }),
-  cache: new InMemoryCache(),
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/graphql",
+  credentials: "include",
 });
 
-export default client;
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("walletToken");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? token : "",
+    },
+  };
+});
+
+// Create Apollo Client instance
+export const createApolloClient = () => {
+  return new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      query: {
+        fetchPolicy: "network-only",
+      },
+      watchQuery: {
+        fetchPolicy: "network-only",
+      },
+    },
+  });
+};
+
+let apolloClient = createApolloClient();
+
+// Function to reset the client
+export const resetApolloClient = () => {
+  apolloClient = createApolloClient();
+  return apolloClient;
+};
+
+export default apolloClient;
