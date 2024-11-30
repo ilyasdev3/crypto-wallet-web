e69f973dddcf1bc9c6fde872917cda5b892a2ea9
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-11-30 20:38:41 +0500

    Add types for user

---
+++ types/user.types.ts
@@ -0,0 +1,13 @@
+export type User = {
+  me: {
+    id: string;
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    profileImage: string;
+    isVerified: boolean;
+    createdAt: string;
+    updatedAt: string;
+  };
+};
