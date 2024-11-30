69a597a0001e77745200fbc47bcf0d1620d5548c
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-11-30 20:43:12 +0500

    Update user query

--- src/graphql/user/queries.user.ts
+++ src/graphql/user/queries.user.ts
@@ -1,1 +1,22 @@
+import { gql } from "@apollo/client";
 
+export const GET_USER = gql`
+  query Query {
+    me {
+      address
+      bio
+      createdAt
+      email
+      firstName
+      followers
+      following
+      id
+      isVerified
+      lastName
+      phone
+      profileImage
+      updatedAt
+      username
+    }
+  }
+`;
