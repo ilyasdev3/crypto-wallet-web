9203fc0618a3662b89b15c3cd88bf8b8d3c438d1
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-12-28 22:45:58 +0500

    add mutation for transfer and withdraw funds

--- src/graphql/wallet/mutation.wallet.ts
+++ src/graphql/wallet/mutation.wallet.ts
@@ -1,1 +1,17 @@
 import { gql } from "@apollo/client";
+
+export const TRANSFER_FUNDS = gql`
+  mutation Mutation($transferFunds: TransferFundsInput!) {
+    transferFunds(transferFunds: $transferFunds) {
+      message
+    }
+  }
+`;
+
+export const WITHDRAW_FUNDS = gql`
+  mutation Mutation($withdrawFunds: WithdrawFundsInput!) {
+    withdrawFunds(withdrawFunds: $withdrawFunds) {
+      message
+    }
+  }
+`;
