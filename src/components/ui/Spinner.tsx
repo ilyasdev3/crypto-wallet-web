16d1f29f26cac6041b32492eb14a89245fcaeed5
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-12-31 22:45:44 +0500

    Add height in spinner to show in buttons

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 
-const Spinner = () => {
+interface SpinnerProps {
+  height?: string;
+}
+
+const Spinner = ({ height }: SpinnerProps) => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div
+      className={`flex items-center justify-center ${
+        height ? height : "min-h-screen"
+      }`}
+    >
       <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-primary-500"></div>
     </div>
   );
