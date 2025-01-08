16f6fd7715f66cf25771293581ea1aedfd0e9583
ilyasdev3 <ilyassanjrani708@gmail.com>
2025-01-08 20:57:03 +0500

    Update profile dropdown

--- src/components/ProfileDropdown.Navbar.tsx
+++ src/components/ProfileDropdown.Navbar.tsx
@@ -11,13 +11,13 @@ const ProfileDropdownNavbar = () => {
       >
         View Profile
       </Link>
-      <Link
+      {/* <Link
         to="/settings"
         className="block px-4 py-2 text-white hover:bg-dark-100 transition"
       >
         Settings
       </Link>
-      <hr className="my-2 border-dark-100" />
+      <hr className="my-2 border-dark-100" /> */}
       <button
         onClick={() => {
           router("/");
