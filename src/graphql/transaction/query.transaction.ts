875d5b49e8b6da4946be4b6fa8dc4f620a065130
ilyasdev3 <ilyassanjrani708@gmail.com>
2025-01-23 22:22:06 +0500

    Update query transaction

--- src/graphql/transaction/query.transaction.ts
+++ src/graphql/transaction/query.transaction.ts
@@ -1,19 +1,28 @@
 import { gql } from "@apollo/client";
 
 export const GET_USER_TRANSACTIONS = gql`
-  query GetUserTransactions($input: UserTransactionInput) {
-    getUserTransactions(input: $input) {
-      address
-      amount
-      contractId
-      createdAt
-      receiverWalletId
-      id
-      senderWalletId
-      status
-      type
-      transactionHash
-      updatedAt
+  query GetUserTransactions(
+    $input: UserTransactionInput!
+    $pagination: PaginationInput = { limit: 10, page: 1 }
+  ) {
+    getUserTransactions(input: $input, pagination: $pagination) {
+      transactions {
+        id
+        amount
+        status
+        senderWalletId
+        receiverWalletId
+        contractId
+        transactionHash
+        type
+        createdAt
+        updatedAt
+      }
+      pageInfo {
+        totalItems
+        totalPages
+        currentPage
+      }
     }
   }
 `;
