b01484bd6a9d8a7b90028ea9e4c91fb22c5d50cf
ilyasdev3 <ilyassanjrani708@gmail.com>
2025-02-01 11:54:27 +0500

    Update

--- src/graphqlClient.ts
+++ src/graphqlClient.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri: "crypto-wallet-f5bom1481-ilyasdev3s-projects.vercel.app",
   credentials: "include",
 });
 
